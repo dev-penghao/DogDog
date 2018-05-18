f84a5ff6e4724f6a85332b3b7a23137e0ada94ba
Penghao <755407349@qq.com>
2018-05-18 22:03:03 +0800

    更新README文件

--- app/src/main/java/com/shiyan/dogdog/MainActivity.java
+++ app/src/main/java/com/shiyan/dogdog/MainActivity.java
@@ -7,14 +7,14 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TableLayout;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,18 +28,89 @@ public class MainActivity extends AppCompatActivity {
     ViewPager viewPager;
     TabLayout tabLayout;
     List<View> viewList;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        viewPager=findViewById(R.id.view_pager);
-        tabLayout=findViewById(R.id.tab_layout);
+        TextView text1=new TextView(MainActivity.this);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+        viewPager=findViewById(R.id.viewpager);
+        tabLayout=findViewById(R.id.tabs);
+
+        tabLayout.addTab(tabLayout.newTab().setText("消息"));
+        tabLayout.addTab(tabLayout.newTab().setText("好友"));
+        tabLayout.addTab(tabLayout.newTab().setText("动态"));
+
+        text1.setText("\n" +
+                "\n" +
+                "    public class MainActivity extends Activity {    \n" +
+                "    private String[] data = { \"Apple\", \"Banana\", \"Orange\", \"Watermelon\",    \n" +
+                "    \"Pear\", \"Grape\", \"Pineapple\", \"Strawberry\", \"Cherry\", \"Mango\" };    \n" +
+                "    @Override    \n" +
+                "    protected void onCreate(Bundle savedInstanceState) {    \n" +
+                "    super.onCreate(savedInstanceState);    \n" +
+                "    setContentView(R.layout.activity_main);    \n" +
+                "    ArrayAdapter<String> adapter = new ArrayAdapter<String>(    \n" +
+                "    MainActivity.this, android.R.layout.simple_list_item_1, data);    \n" +
+                "    ListView listView = (ListView) findViewById(R.id.list_view);    \n" +
+                "    listView.setAdapter(adapter);    \n" +
+                "    }    \n" +
+                "    }    \n" +
+                "\n" +
+                "    public class MainActivity extends Activity {    \n" +
+                "    private String[] data = { \"Apple\", \"Banana\", \"Orange\", \"Watermelon\",    \n" +
+                "    \"Pear\", \"Grape\", \"Pineapple\", \"Strawberry\", \"Cherry\", \"Mango\" };    \n" +
+                "    @Override    \n" +
+                "    protected void onCreate(Bundle savedInstanceState) {    \n" +
+                "    super.onCreate(savedInstanceState);    \n" +
+                "    setContentView(R.layout.activity_main);    \n" +
+                "    ArrayAdapter<String> adapter = new ArrayAdapter<String>(    \n" +
+                "    MainActivity.this, android.R.layout.simple_list_item_1, data);    \n" +
+                "    ListView listView = (ListView) findViewById(R.id.list_view);    \n" +
+                "    listView.setAdapter(adapter);    \n" +
+                "    }    \n" +
+                "    }    \n" +
+                "\n" +
+                "    public class MainActivity extends Activity {    \n" +
+                "    private String[] data = { \"Apple\", \"Banana\", \"Orange\", \"Watermelon\",    \n" +
+                "    \"Pear\", \"Grape\", \"Pineapple\", \"Strawberry\", \"Cherry\", \"Mango\" };    \n" +
+                "    @Override    \n" +
+                "    protected void onCreate(Bundle savedInstanceState) {    \n" +
+                "    super.onCreate(savedInstanceState);    \n" +
+                "    setContentView(R.layout.activity_main);    \n" +
+                "    ArrayAdapter<String> adapter = new ArrayAdapter<String>(    \n" +
+                "    MainActivity.this, android.R.layout.simple_list_item_1, data);    \n" +
+                "    ListView listView = (ListView) findViewById(R.id.list_view);    \n" +
+                "    listView.setAdapter(adapter);    \n" +
+                "    }    \n" +
+                "    }    \n" +
+                "\n" +
+                "    public class MainActivity extends Activity {    \n" +
+                "    private String[] data = { \"Apple\", \"Banana\", \"Orange\", \"Watermelon\",    \n" +
+                "    \"Pear\", \"Grape\", \"Pineapple\", \"Strawberry\", \"Cherry\", \"Mango\" };    \n" +
+                "    @Override    \n" +
+                "    protected void onCreate(Bundle savedInstanceState) {    \n" +
+                "    super.onCreate(savedInstanceState);    \n" +
+                "    setContentView(R.layout.activity_main);    \n" +
+                "    ArrayAdapter<String> adapter = new ArrayAdapter<String>(    \n" +
+                "    MainActivity.this, android.R.layout.simple_list_item_1, data);    \n" +
+                "    ListView listView = (ListView) findViewById(R.id.list_view);    \n" +
+                "    listView.setAdapter(adapter);    \n" +
+                "    }    \n" +
+                "    }    ");
 
         LayoutInflater inflater=getLayoutInflater();
-        view1=inflater.inflate(R.layout.view1,null);
+        view1=text1;
         view2=inflater.inflate(R.layout.view2,null);
         view3=inflater.inflate(R.layout.view3,null);
+
 
         viewList=new ArrayList<>();
         viewList.add(view1);
@@ -47,7 +118,6 @@ public class MainActivity extends AppCompatActivity {
         viewList.add(view3);
 
         viewPager.setAdapter(adapter);
-
 
 //            new Thread(new Runnable() {
 //                @Override
