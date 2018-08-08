88922de46cd34a19e36d021aeeb69e682ccea422
Penghao <755407349@qq.com>
2018-08-08 18:45:31 +0800

    删去了联系人界面能显示新消息的功能

--- app/src/main/java/com/shiyan/fragment/ContactsFragment.java
+++ app/src/main/java/com/shiyan/fragment/ContactsFragment.java
@@ -39,22 +39,21 @@ public class ContactsFragment extends Fragment {
     Button button;
     RecyclerView contacts;
     SwipeRefreshLayout refreshLayout;
-    List<String[]> friendList=new ArrayList<>();// 数组会有3个元素，0是昵称，1是账号，2是消息
+    List<String[]> friendList = new ArrayList<>();// 数组会有3个元素，0是昵称，1是账号，2是消息
 
     ContactsAdapter contactsAdapter;
-    ContactsBroadcast contactsBroadcast;
     Context context;
 
     @SuppressLint("HandlerLeak")
-    Handler handler=new Handler(){
+    Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
-            if (msg.what==0){
-                for (String[] ss:friendList){
-                    Log.d("注意",ss[0]+" : "+ss[1]);
+            if (msg.what == 0) {
+                for (String[] ss : friendList) {
+                    Log.d("注意", ss[0] + " : " + ss[1]);
                 }
-                for (int i=0;i<friendList.size();i++){
+                for (int i = 0; i < friendList.size(); i++) {
                     contactsAdapter.notifyItemChanged(i);// 这里不能使用Insert,否则会有奇怪的BUG
                 }
                 refreshLayout.setRefreshing(false);
@@ -65,55 +64,51 @@ public class ContactsFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        context=getContext();
-        IntentFilter filter=new IntentFilter();
-        filter.addAction("new_message");
-        contactsBroadcast=new ContactsBroadcast();
-        context.registerReceiver(contactsBroadcast,filter);
+        context = getContext();
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.view2,container,false);
-        RecyclerView.LayoutManager layoutManager=new LinearLayoutManager(getContext());
+        View view = inflater.inflate(R.layout.view2, container, false);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
 
-        contacts=view.findViewById(R.id.view2_contacts_recyclerview);
-        refreshLayout=view.findViewById(R.id.view2_swipeRefreshLayout);
-        button=view.findViewById(R.id.view2_button);
+        contacts = view.findViewById(R.id.view2_contacts_recyclerview);
+        refreshLayout = view.findViewById(R.id.view2_swipeRefreshLayout);
+        button = view.findViewById(R.id.view2_button);
 
         contacts.setLayoutManager(layoutManager);
-        contactsAdapter=new ContactsAdapter();
+        contactsAdapter = new ContactsAdapter();
         contacts.setAdapter(contactsAdapter);
         refreshLayout.setOnRefreshListener(this::flushFriendList);
         button.setOnClickListener(v -> flushFriendList());
         return view;
     }
 
-    public void flushFriendList(){
-        Request request=new Request();
-        request.putType("get_friend_list");
-        request.putContent(Me.num);
+    public void flushFriendList() {
         new Thread(() -> {
-            String result=request.sendRequest();
+            Request request = new Request();
+            request.putType("get_friend_list");
+            request.putContent(Me.num);
+            String result = request.sendRequest();
             try {
-                List<String> lastMessage=new ArrayList<>();
-                if (!friendList.isEmpty()){
-                    for (int i=0;i<friendList.size();i++){
+                List<String> lastMessage = new ArrayList<>();
+                if (!friendList.isEmpty()) {
+                    for (int i = 0; i < friendList.size(); i++) {
                         lastMessage.add(friendList.get(i)[2]);
                     }
                 }
                 friendList.clear();
-                JSONArray jsonArray=new JSONArray(result);
-                for (int i=0;i<jsonArray.length();i++){
-                    JSONObject obj=jsonArray.getJSONObject(i);
-                    String[] user=new String[3];
-                    user[0]=obj.getString("name");
-                    user[1]=obj.getString("num");
-                    if (!lastMessage.isEmpty()){
-                        user[2]=lastMessage.get(i);
+                JSONArray jsonArray = new JSONArray(result);
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject obj = jsonArray.getJSONObject(i);
+                    String[] user = new String[3];
+                    user[0] = obj.getString("name");
+                    user[1] = obj.getString("num");
+                    if (!lastMessage.isEmpty()) {
+                        user[2] = lastMessage.get(i);
                     }
-                    friendList.add(i,user);
+                    friendList.add(i, user);
                 }
                 handler.sendEmptyMessage(0);
             } catch (JSONException e) {
@@ -122,30 +117,25 @@ public class ContactsFragment extends Fragment {
         }).start();
     }
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        context.unregisterReceiver(contactsBroadcast);
-    }
+    class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.ViewHolder> {
 
-    class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.ViewHolder>{
-
-        class ViewHolder extends RecyclerView.ViewHolder{
+        class ViewHolder extends RecyclerView.ViewHolder {
             TextView userName;
             TextView content;
             View itemView;
+
             ViewHolder(View itemView) {
                 super(itemView);
-                this.itemView=itemView;
-                userName=itemView.findViewById(R.id.contacts_item_user_name);
-                content=itemView.findViewById(R.id.contacts_item_content);
+                this.itemView = itemView;
+                userName = itemView.findViewById(R.id.contacts_item_user_name);
+                content = itemView.findViewById(R.id.contacts_item_content);
             }
         }
 
         @NonNull
         @Override
         public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            View view=LayoutInflater.from(parent.getContext()).inflate(R.layout.contacts_item_view,parent,false);
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.contacts_item_view, parent, false);
             return new ViewHolder(view);
         }
 
@@ -154,11 +144,11 @@ public class ContactsFragment extends Fragment {
             holder.userName.setText(friendList.get(position)[0]);
             // 如果有第三项
 //            if (friendList.get(position).length>2){
-                holder.content.setText(friendList.get(position)[2]);
+            holder.content.setText(friendList.get(position)[2]);
 //            }
             holder.itemView.setOnClickListener(v -> {
-                Intent intent=new Intent(getActivity(), TalkActivity.class);
-                intent.putExtra("num",friendList.get(position)[1]);
+                Intent intent = new Intent(getActivity(), TalkActivity.class);
+                intent.putExtra("num", friendList.get(position)[1]);
                 startActivity(intent);
             });
         }
@@ -167,21 +157,6 @@ public class ContactsFragment extends Fragment {
         public int getItemCount() {
             return friendList.size();
         }
-    }
 
-    class ContactsBroadcast extends BroadcastReceiver{
-
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            if ("new_message".equals(intent.getAction())){
-                String[] ss=intent.getStringExtra("new").split("/");// 0是发送者，1是消息内容
-                for (int i=0;i<friendList.size();i++){
-                    if (ss[0].equals(friendList.get(i)[1])){
-                        friendList.get(i)[2]=ss[1];
-                        contactsAdapter.notifyItemChanged(i);
-                    }
-                }
-            }
-        }
     }
 }
